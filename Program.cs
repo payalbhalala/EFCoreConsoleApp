67a45d3513d8f972f0c7f0dfc854d22b00c76d20
payalbhalala <pkb7@njit.edu>
2021-06-11 19:24:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EFCoreTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+
+                var std = new Student()
+                {
+                    Name = "Bill"
+                };
+
+                context.Students.Add(std);
+                context.SaveChanges();
+            }
+        }
+    }
+}
+public class Student
+{
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+}
+
+public class Course
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+}
+namespace EFCoreTutorials
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
